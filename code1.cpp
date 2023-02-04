18a0b6c5e9e86b55810424ec4396056caf5214d1
Snehagupta26 <105607723+Snehagupta26@users.noreply.github.com>
2023-02-04 23:51:21 +0530

    Add files via upload

---
+++ code1.cpp
@@ -0,0 +1,21 @@
+//starting with linked list
+//implementation of node in singly linked list
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+int main(){
+ Node* Node1 =  new Node(10);
+cout<<Node1->data<<endl;
+cout<<Node1->next<<endl;
+
+ return 0;
+}
