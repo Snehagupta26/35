924601e846c8e7a2a8bff11eee73f826007a7138
Snehagupta26 <105607723+Snehagupta26@users.noreply.github.com>
2023-11-16 22:44:24 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int fac(int n){
+    if(n==0){
+        return 1;
+    }
+    return n*fac(n-1);
+}
+int main(){
+    int n;
+    cin>>n;
+    int ans = fac(n);
+    cout<<ans;
+
+}
